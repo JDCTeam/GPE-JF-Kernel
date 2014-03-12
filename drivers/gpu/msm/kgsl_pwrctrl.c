c453842bb8e25a20db95de8ebfe8db13515a9a8a
flar2 <asegaert@gmail.com>
2014-03-11 20:21:33 -0400

    ElementalX Governor
    Signed-off-by: flar2 <asegaert@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -38,6 +38,10 @@
 struct device *stored_dev;
 #endif
 
+#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
+int graphics_boost = 2;
+#endif
+
 struct clk_pair {
 	const char *name;
 	uint map;
@@ -178,6 +182,10 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	}
 
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel, pwrlevel->gpu_freq);
+
+#ifdef CONFIG_CPU_FREQ_GOV_ELEMENTALX
+        graphics_boost = pwr->active_pwrlevel;
+#endif
 }
 
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
