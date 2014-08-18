4cdaf75d49259b911b44f9ebdfd003c58afbf434
Roman Birg <roman@cyngn.com>
2014-08-18 14:04:44 -0700

    jf: report SW_LID instead of SW_FLIP
    Change-Id: Iebffbabdbb3748eec4f887ebd227c67adf01d8ef
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Scott Mertz <smertz@cyngn.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -640,7 +640,7 @@ static void flip_cover_work(struct work_struct *work)
 			__func__, ddata->flip_cover);
 
 		input_report_switch(ddata->input,
-			SW_FLIP, ddata->flip_cover);
+			SW_LID, ddata->flip_cover);
 		input_sync(ddata->input);
 	} else {
 		printk(KERN_DEBUG "%s : Value is not same!\n", __func__);
@@ -650,7 +650,7 @@ static void flip_cover_work(struct work_struct *work)
 	
 	printk(KERN_DEBUG "[keys_no_delay] %s : %d\n",__func__, ddata->flip_cover);
 
-	input_report_switch(ddata->input, SW_FLIP, ddata->flip_cover);
+	input_report_switch(ddata->input, SW_LID, ddata->flip_cover);
 	input_sync(ddata->input);
 #endif
 }
@@ -941,7 +941,7 @@ static int __devinit gpio_keys_probe(struct platform_device *pdev)
 	input->dev.parent = &pdev->dev;
 #ifdef CONFIG_SENSORS_HALL
 	input->evbit[0] |= BIT_MASK(EV_SW);
-	input_set_capability(input, EV_SW, SW_FLIP);
+	input_set_capability(input, EV_SW, SW_LID);
 #endif
 	input->open = gpio_keys_open;
 	input->close = gpio_keys_close;
