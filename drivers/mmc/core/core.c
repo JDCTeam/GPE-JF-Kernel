db9c8d66d640ba9f9d6b82eacc2a297f473ca319
Paul Keith <javelinanddart@gmail.com>
2015-12-15 23:15:20 -0800

    mmc: Export CRC Toggle to Sysfs
    Change-Id: Ibae84d2d944fe79e03f84ccdb4ee031217e05430
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -69,10 +69,8 @@ static struct workqueue_struct *workqueue;
  * performance cost, and for other reasons may not always be desired.
  * So we allow it it to be disabled.
  */
-
 bool use_spi_crc = 0;
-
-module_param(use_spi_crc, bool, 0);
+module_param(use_spi_crc, bool, 0755);
 
 /*
  * We normally treat cards as removed during suspend if they are not
